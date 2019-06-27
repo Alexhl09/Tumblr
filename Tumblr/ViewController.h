05dc345b9a5ac18aa8dac182b33fe74e2c991d79
Alejandro Hernandez Lopez <alexhl09@fb.com>
2019-06-27 10:29:15 -0700

    Initial Commit

---
+++ Tumblr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tumblr
+//
+//  Created by alexhl09 on 6/27/19.
+//  Copyright © 2019 alexhl09. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tumblr/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tumblr
+//
+//  Created by alexhl09 on 6/27/19.
+//  Copyright © 2019 alexhl09. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
